54dd84d6dabada48497d6e024831d5cafa419457
Aniketh <anikethsdeshpande@gmail.com>
2019-08-31 00:21:13 +0530

    s1 is a service that sends Q details

---
+++ s1.py
@@ -0,0 +1,32 @@
+# Author: Aniketh S Deshpande
+# Flask-Server
+
+from flask import Flask, Request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+# f is a flower name
+q = {["f1": {
+        "size": 4
+    },
+    "f2": {
+        "size": 6
+    },
+    "f3": {
+        "size": 2
+    }
+]}
+
+class Q_insert(Resource):
+    global q
+    def get(self):
+        return q
+
+# resources routing
+api.add_resource(Q_insert, '/q_insert')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
